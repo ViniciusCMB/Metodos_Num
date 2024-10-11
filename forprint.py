3c8dbcf12347266436cb7aff3a2ff2ce83b6f537
ViniciusCMB <viniciusmonnerat958@gmail.com>
2024-10-11 17:20:47 -0300

    Commit

--- forprint.py
+++ forprint.py
@@ -9,16 +9,15 @@ def sistema(x, t):
     dx2dt = (1000*np.exp((-10)/(f2+273))*f1)-(10*(f2-20))
     return np.array([dx1dt, dx2dt])  # Corrigido para retornar um array numpy corretamente
 
-Tini = 35  # temperatura inicial em graus Celsius
-Cini = 5  # concentração inicial em gmol/L
+Tini = 20  # temperatura inicial em graus Celsius
+Cini = 10  # concentração inicial em gmol/L
 
 x0 = [Cini, Tini]  # vetor de condições iniciais
-t = [np.linspace(0, 10, 51), np.linspace(0, 10, 101), np.linspace(0, 10, 1001)]  # vetor de tempo de 0 a 10 segundos com diferentes resoluções
+t = np.linspace(0, 10, 1001)  # vetor de tempo de 0 a 10 segundos com diferentes resoluções
 
-solucao = [rk4(sistema, x0, tempo) for tempo in t]
+solucao = rk4(sistema, x0, t)
 
-for tempo, sol in zip(t, solucao):
-    plt.plot(tempo, sol[:, 1], label=f'Temperatura do reator T(t) com {tempo[-1]/(len(tempo)-1)} de passo')  # plota o gráfico da temperatura
+plt.plot(t, solucao[:, 1], label=f'Temperatura do reator T(t) com {t[-1]/(len(t)-1)} de passo')  # plota o gráfico da temperatura
 
 plt.xlabel('Tempo (s)')
 plt.ylabel('Temperatura (K)')
